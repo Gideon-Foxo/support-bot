a09345f5441ddb60dc6cfe9ced23a4f05ee86868
Gideon <gideonfordmiles@gmail.com>
2021-05-27 00:57:07 -0400

    Added bot version to the ping command

--- src/commands/information/ping.js
+++ src/commands/information/ping.js
@@ -2,10 +2,10 @@ module.exports = {
 
 	name: 'ping',
 	description: 'Ping!',
-	execute(msg, args) {
+	execute(msg, args, stuff) {
 
 		// Pong!
-		msg.channel.send('Loading...').then(sentMessage => sentMessage.edit(`Pong! 🏓\nPing: **${Math.round(msg.client.ws.ping)}**ms | API: **${sentMessage.createdTimestamp - msg.createdTimestamp}**ms`));
+		msg.channel.send('Loading...').then(sentMessage => sentMessage.edit(`Pong! 🏓 Version **${stuff.pack.version}**\nPing: **${Math.round(msg.client.ws.ping)}**ms | API: **${sentMessage.createdTimestamp - msg.createdTimestamp}**ms`));
 	
 	},
 };
